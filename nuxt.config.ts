0ddf4b636a909ddc14843b371de2e66dc1eef428
Niklas Haag <niklas.haag@yahoo.de>
2023-03-20 16:08:52 +0100

    Install mdi fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['@mdi/font/css/materialdesignicons.min.css'],
   build: {
     transpile: ['vuetify'],
   },
